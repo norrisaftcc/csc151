e77e17ee3ae16ae4b7711b5237fa09431ffe3527
teacherbot <norrisa@faytechcc.edu>
2025-03-06 16:01:29 +0000

    Version with user input

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 
 import java.io.*;
+import java.util.Scanner;
 
 // By implementing Serializable interface
 // we make sure that state of instances of class DogStepTracker
@@ -9,30 +10,61 @@ class DogStepTracker implements Serializable {
     
     int steps;
     String dogName;
+    String dogBreed;
+    String owner;
 
     // DogStepTracker class constructor
-    public DogStepTracker(int steps, String dogName) {
+    public DogStepTracker(int steps, String dogName, String dogBreed, String owner) {
         this.steps = steps;
         this.dogName = dogName;
+        this.dogBreed = dogBreed;
+        this.owner = owner;
     }
 }
 
 public class Test {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        DogStepTracker tracker = new DogStepTracker(5000, "Buddy");
+        // TODO: get all the info
+
+        /*
+        int steps = 100;
+        String dogName = "Esther";
+        String dogBreed = "Lab Mix";
+        String owner = "norrisa";
+        */
+        // Ask the User to type the info
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of steps: ");
+        int steps = scanner.nextInt();
+        scanner.nextLine();  // Consume newline
+
+        System.out.print("Enter the dog's name: ");
+        String dogName = scanner.nextLine();
+
+        System.out.print("Enter the dog's breed: ");
+        String dogBreed = scanner.nextLine();
+
+        System.out.print("Enter the owner's name: ");
+        String owner = scanner.nextLine();
+        // cleanup
+        scanner.close();
+        
+        DogStepTracker tracker = new DogStepTracker(steps, dogName, dogBreed, owner);
 
         // Serializing 'tracker'
-        FileOutputStream fos = new FileOutputStream("dog_tracker.txt");
+        FileOutputStream fos = new FileOutputStream("dog_tracker.data");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(tracker);
 
         // De-serializing 'tracker'
-        FileInputStream fis = new FileInputStream("dog_tracker.txt");
+        FileInputStream fis = new FileInputStream("dog_tracker.data");
         ObjectInputStream ois = new ObjectInputStream(fis);
         DogStepTracker deserializedTracker = (DogStepTracker) ois.readObject(); // down-casting object
 
         System.out.println("Dog Name: " + deserializedTracker.dogName + ", Steps: " + deserializedTracker.steps);
-
+        System.out.println("Owner: " + deserializedTracker.owner);
+        // TODO: print all the other data 
         // closing streams
         oos.close();
         ois.close();
