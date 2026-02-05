ddd860c52c33e141d1f9127bf0d69124136b8895
teacherbot <norrisa@faytechcc.edu>
2026-02-05 18:40:02 +0000

    added fake dice roll

--- Demo.java
+++ Demo.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Demo {
+
+    public static int roll() {
+        // roll a die, return from 1 to 6
+        // for now just pretend
+        return 1;
+    }
     public static void main (String[] args) {
         System.out.println("Hey, what's your name?");
         Scanner input = new Scanner(System.in);
@@ -8,6 +14,12 @@ public class Demo {
 
         System.out.println();
         System.out.println("Nice to meetcha, " + name);
+        int chips = 100;
+        System.out.println("Well," + name + " you start with " + chips + " chips.");
+        System.out.println();
+        System.out.println("Rolling the 🎲 dice... ");
+        int roll = roll();
+        System.out.println("You rolled a: " + roll);
     }
 
 }
