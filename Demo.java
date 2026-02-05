ed855faef783a9115c5deb3ac47e674d68e44f7d
teacherbot <norrisa@faytechcc.edu>
2026-02-05 18:59:04 +0000

    worlds simplest craps game

--- Demo.java
+++ Demo.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Demo {
 
     public static int roll() {
         // roll a die, return from 1 to 6
-        // for now just pretend
-        return 1;
+        Random roller = new Random();
+        int roll = roller.nextInt(5) + 1; // 0-5 becomes 1-6
+        return roll;
     }
     public static void main (String[] args) {
         System.out.println("Hey, what's your name?");
@@ -17,9 +19,14 @@ public class Demo {
         int chips = 100;
         System.out.println("Well," + name + " you start with " + chips + " chips.");
         System.out.println();
-        System.out.println("Rolling the 🎲 dice... ");
-        int roll = roll();
-        System.out.println("You rolled a: " + roll);
+        // Loop
+        for (int i=0; i < 20; i++) {
+            System.out.println("Rolling two 🎲 dice... ");
+            int roll = roll() + roll();
+            System.out.println("You rolled a: " + roll);
+        }
+        // end of program, close the input
+        input.close();
     }
 
 }
